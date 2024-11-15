bfe57512c68f2dafe3b05e117045bac04a953c6c
Evgenia Loshakova <janelo1984@yandex.ru>
2024-11-15 12:15:22 +0300

    Larek 8.2

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,12 +11,12 @@ interface IMainPage {
 
 // Интерфейс карточки товара
 interface IProductItem {
-id: string
-description?: string
-image: string
-title: string
-category: string
-price: number | null
+    id: string
+    description?: string
+    image: string
+    title: string
+    category: string
+    price: number | null
 }
 
 // Интерфейс, описывающий корзину
@@ -28,8 +28,7 @@ interface IBasketModel {
 
 // Интерфейс, описывающий данные заказа и покупателя
 interface IOrderData {
-    items: string | string[] 
-    total: number
+    items: string[] 
     paymentMethod: string
     address: string
     email: string
@@ -43,30 +42,28 @@ interface IAddressForm {
   }
 
 // Интерфейс, описывающий Форму (модальное окно) с контактами покупателя
-  interface IContactsForm {
+interface IContactsForm {
     phone: string
     email: string
   }
 
   // Интерфейс, описывающий Форму (модальное окно) успешного оформления заказа
-  interface IOrderSuccess {
-    total: number
+interface IOrderSuccess {
+    paymentConfirmation: number
   }
 
   // Интерфейс, описывающий текущее состояние приложения
 interface IAppState {
-basketCounter: number
-catalog: HTMLElement[]
-basket: HTMLElement | HTMLElement[]
-orderData: IOrderData 
-formErrors: FormErrors
-setCatalog(items: IProductItem[]): void
-addToBasket(item: HTMLElement): void   // добавить в корзину товар
-deleteFromBasket(id: string): void  // удалить из корзины товар
-cleanBasket(): void            // полностью очистить корзину
-getBasketCounter(): number    // калькулятор количества товаров в корзине
-getBasketTotal(): number      // калькулятор общей стоимости товаров в корзине
-setOrderField(field: keyof IOrderData, value: string): void    // метод для заполнения полей Форм с адресом и контактами
-validateAddressForm(): boolean  // валидация Формы с адресом покупателя и методом оплаты
-validateContactsForm(): boolean // валидация Формы с контактами покупателя
+    catalog: IProductItem[]
+    basket: IProductItem[]
+    orderData: IOrderData 
+    formErrors: FormErrors
+    addToBasket(item: HTMLElement): void   // добавить в корзину товар
+    deleteFromBasket(id: string): void  // удалить из корзины товар
+    cleanBasket(): void            // полностью очистить корзину
+    getBasketCounter(): number    // калькулятор количества товаров в корзине
+    getBasketTotal(): number      // калькулятор общей стоимости товаров в корзине
+    setOrderField(field: keyof IOrderData, value: string): void    // метод для заполнения полей Форм с адресом и контактами
+    validateAddressForm(): boolean  // валидация Формы с адресом покупателя и методом оплаты
+    validateContactsForm(): boolean // валидация Формы с контактами покупателя
 }
